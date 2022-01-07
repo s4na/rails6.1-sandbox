ab2c50ac31a181f2d2f9f00da4a88b3ffb0683c3
s4na <appletea.umauma@gmail.com>
2022-01-07 13:37:11 +0900

    s

--- app/models/foo.rb
+++ app/models/foo.rb
@@ -1,3 +1,13 @@
 class Foo < ApplicationRecord
   belongs_to :hoge
+
+  validate :check_hoge_id
+
+  def check_hoge_id
+    # validate時のhogeの参照先を確認
+
+    p 'hoge'
+    p hoge
+    p '---'
+  end
 end
--- test/models/foo_test.rb
+++ test/models/foo_test.rb
@@ -13,4 +13,20 @@ class FooTest < ActiveSupport::TestCase
     assert foo.save
     assert_equal hoge, foo.hoge
   end
+
+  test "#update" do
+    hoge = Hoge.new
+    assert hoge.save
+    foo = Foo.new(hoge: hoge)
+    assert foo.save
+    assert_equal hoge, foo.hoge
+
+    hoge2 = Hoge.new(id: 2)
+    assert hoge2.save
+
+    p '============='
+
+    # エラーが出ないか確認
+    foo.update!(hoge_id: hoge2.id)
+  end
 end
